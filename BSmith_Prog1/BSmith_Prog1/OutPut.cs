09c88d109da0875bcbd66a80336b5a1d2222c45b
Blaine Smith <Smith.blaine1993@gmail.com>
2018-06-14 20:58:59 -0500

    Add files via upload

---
+++ BSmith_Prog1/BSmith_Prog1/OutPut.cs
@@ -0,0 +1,39 @@
+﻿/* Blaine Smith 
+ * Program 1 Due: Jan-16-2018
+ * None 
+ * None 
+ * This code will produce three different Outputs with one quote,
+ * in different ways.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace BSmith_Prog1
+{
+    class OutPut
+    {
+        static void Main(string[] args)
+        {
+                Write("Output #1\n");
+                Write("\tLife is change. Growth is optional. Choose Wisely.\n");
+
+                Write("\nOutPut #2\n");
+                Write("\tLife is change.\n");
+                Write("\tGrowth is optional.\n");
+                Write("\tChoose Wisely.\n");
+
+                Write("\nOutPut #3\n");
+                Write("\tLife is\n");
+                Write("\tchange.\n");
+                Write("\tGrowth is\n");
+                Write("\toptional\n");
+                Write("\tChoose\n");
+                Write("\tWisely\n");
+            }
+        }
+    }
+
